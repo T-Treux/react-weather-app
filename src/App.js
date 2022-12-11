3f244a5fb04a01e1da0e857d8bf6e8e17f3733f5
T-Treux <110358968+T-Treux@users.noreply.github.com>
2022-12-11 12:17:50 -0600

    Added Github

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was created by Tiffannee Wallace and is{" "}
+        <a
+          href="https://app.netlify.com/sites/unrivaled-tiramisu-c7e2b0/overview"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
