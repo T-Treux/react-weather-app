943bce5d8c86331570ea3e12604c8c304eb35a9b
T-Treux <110358968+T-Treux@users.noreply.github.com>
2022-12-11 16:03:37 -0600

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import "./App.css";
+import React from "react";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was created by Tiffannee Wallace and is{" "}
-        <a
-          href="https://app.netlify.com/sites/unrivaled-tiramisu-c7e2b0/overview"
-          target="_blank"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was created by Tiffannee Wallace and is{" "}
+          <a
+            href="https://app.netlify.com/sites/unrivaled-tiramisu-c7e2b0/overview"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>Chicago</h1>
+      <ul>
+        <li>Sunday 1:43 PM</li>
+        <li>Cloudy</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+              alt="Cloudy"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">38</span>
+              <span className="units">°F</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 1%</li>
+            <li> Humidity: 72%</li>
+            <li> Wind: 5 mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
