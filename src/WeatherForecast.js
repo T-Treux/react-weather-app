8dfb5746bb2cf6e76efb34e5e87180ca2261b39f
T-Treux <110358968+T-Treux@users.noreply.github.com>
2022-12-17 17:45:59 -0600

    Changed apikey/url - added useeffect  & styled app

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -7,31 +7,20 @@ export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
 
-  useEffect(function() => {
-setLoaded(false);
+  useEffect(() => {
+    setLoaded(false);
   }, [props.coordinates]);
 
-  
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
 
- 
-  function load(){
- let apiKey = "b08df0a90df1f337ca95e7ecc2d6fadd";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleResponse);
-}
-
-if (loaded) {
+  if (loaded) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          {forecast.forEach(function (dailyForecast, index) {
+          {forecast.map(function (dailyForecast, index) {
             if (index < 5) {
               return (
                 <div className="col" key={index}>
@@ -46,7 +35,13 @@ if (loaded) {
       </div>
     );
   } else {
-   load();
+    let apiKey = "3fdc8cfbf2d6fa0116c9ae92d3df4f79";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(handleResponse);
 
     return null;
   }
